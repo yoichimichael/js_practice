55b881d70844037a934b2f95e97f09fe682abe20
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-04-11 23:42:29 -0400

    add setTimeout function to play.js

--- play.js
+++ play.js
@@ -6,3 +6,15 @@ const fetchData = () => {
   });
   return promise;
 }
+
+setTimeout(() => {
+  console.log('Timer is done!');
+  fetchData()
+    .then(text => {
+      console.log(text);
+      return fetchData();
+    })
+    .then(text2 => {
+      console.log(text2);
+    });
+}, 2000);
