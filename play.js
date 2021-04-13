813f72b9acfad38279ffd308970420f67d5c40ff
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-04-13 01:34:10 -0400

    add custom promise creator with randomized success or failure

--- play.js
+++ play.js
@@ -35,3 +35,23 @@ setTimeout(() => {
 
 console.log('Hello!');
 console.log('Hi!');
+
+
+let prom = new Promise((resolve, reject) => {
+  let num = Math.random();
+  if (num < .5 ){
+    resolve('Yay!');
+  } else {
+    reject('Ohhh noooo!');
+  }
+});
+ 
+const handleSuccess = (resolvedValue) => {
+  console.log(resolvedValue);
+};
+ 
+const handleFailure = (rejectionReason) => {
+  console.log(rejectionReason);
+};
+ 
+prom.then(handleSuccess, handleFailure);
